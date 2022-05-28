89bf87071e0bc3369c2fa36a3c83f720ea9534d8
Korobko-v <19kvd95@mail.ru>
2022-05-28 20:23:13 +0300

    remove redundant imports

--- src/main/java/ru/sk/millionaire/repository/ResultRepository.java
+++ src/main/java/ru/sk/millionaire/repository/ResultRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.sk.millionaire.model.Result;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Integer> {
 
